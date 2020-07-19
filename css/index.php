2a4f602205f09199688cebd9fe524d6f51ed7ce1
Klaudijus <klaudijus.k1996@gmail.com>
2020-07-20 00:51:36 +0300

    Thonk

---
+++ css/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <main class="main">
+        <div class="container">
+            <header class="head">
+                <div class="back">
+                    <
+                </div>
+                <div class="close">
+                    X
+                </div>
+            </header>
+            <div class="path">
+                <p>c:/ayaya/ayaya</p>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
