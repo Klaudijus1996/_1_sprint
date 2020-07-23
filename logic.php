8beb458e8b3f70d3e28fcff173768efd95e060b5
Klaudijus <klaudijus.k1996@gmail.com>
2020-07-23 20:27:25 +0300

    some style updts

--- logic.php
+++ logic.php
@@ -1,4 +1,12 @@
-<?php declare(strict_types = 1); ?>
+<?php declare(strict_types = 1);
+function _sort_entries($a, $b)
+{
+    if ($a->type!=$b->type)
+        return strcmp($a->type,$b->type);
+
+    return strcmp($a->entry,$b->entry);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,11 +22,12 @@
         require_once('upload.php');
         require_once('download.php');
         ?>
+        <div class="bg-img"></div>
         <div class="container">
             <div class="head"></div>
             <?php 
                 ob_start();
-                # render directory ::
+                # Render Files/Folders :: START ::
                 $path = getcwd();
                 $root_path = getcwd();
 
@@ -31,77 +40,89 @@
                     }
                     $path = getcwd();
                     $dirContent = scandir(getcwd());
+                    // usort($dirContent, "_sort_entries");
                     foreach($dirContent as $files) {
                         if ($files == '.' || $files == '..' || $files == '.git') {
                             continue;
-                        } else if (!is_dir($files)) {
-                            if (is_readable($files)) {
-                                echo "
-                                    <div class='filesAndFolders'>
-                                        <form action='' method='get'>
-                                            <input type='submit' value='$files''>
-                                            <input type='hidden' name='path' value='$getPath/$files'>
-                                        </form>
-                                    </div>
-                                    ";
-                                echo "
-                                    <div class='download'>
-                                        <form action='' method='post'>
-                                            <input type='submit' value='Download'>
-                                            <input type='hidden' name='download' value='$getPath/$files'>
-                                        </form>
-                                    </div>
-                                    ";
-                            }
-                        } else {
+                        } else if (is_dir($files)) {
                             echo "
                                 <div class='filesAndFolders'>
                                     <form action='' method='get'>
-                                        <input type='submit' value='$files''>
+                                        <input class='folders' type='submit' value='$files''>
                                         <input type='hidden' name='path' value='$getPath/$files'>
+                                    </form>
+                                </div>
+                                ";
+                        } else if (is_readable($files)) {
+                            echo "
+                                <div class='filesAndFolders'>
+                                    <form action='' method='get'>
+                                        <input class='files' type='submit' value='$files''>
+                                        <input type='hidden' name='path' value='$getPath/$files'>
+                                    </form>
+                                </div>
+                                ";
+                            echo "
+                                <div class='downloads'>
+                                    <form action='' method='post'>
+                                        <input class='download' type='submit' value='download'>
+                                        <input type='hidden' name='download' value='$getPath/$files'>
                                     </form>
                                 </div>
                                 ";
                         }
                     }
                 }
-                # Render directory END;
-                # Render back && back to start
+                # Render Files/Folders :: END :: ;
+                # Render back && back to start :: START ::
                 $getPath = $_GET['path'];
                 if (isset($_GET["path"])) {
                         scan_dir($path);
+                        // echo usort(scan_dir($path), "_sort_entries");
                         if (is_file($path.$getPath)) {
                             ob_clean();
                             read($path.$getPath);
                         } 
                         $str = substr($getPath, stripos($getPath, '/'), strripos($getPath, '/'));
+                        if (is_file($path.$_GET['path'])) {
+                            echo "
+                                <form class='exit' action='' method='get'>
+                                    <input class='exit-btn' type='submit' name='reset' value='Exit'>
+                                </form>
+                                ";
+                        } else
                         if ($str == "") {
                             echo "
-                            <form action='' method='get'>
-                                <input type='submit' name='reset' value='Back to Start'>
+                            <form class='back-to-start' action='' method='get'>
+                                <input class='bst-btn' type='submit' name='reset' value='&#171;'>
                             </form>
                             "; 
                         } else {
                         echo "
-                            <form action='' method='get'>
-                                <input type='submit' value='Back'>
+                            <form class='back' action='' method='get'>
+                                <input class='back-btn' type='submit' value='Back'>
                                 <input type='hidden' name='path' value='$str'>
                             </form>
                             "; 
                             echo "
-                            <form action='' method='get'>
-                                <input type='submit' name='reset' value='Back to Start'>
+                            <form class='back-to-start' action='' method='get'>
+                                <input class='bst-btn' type='submit' name='reset' value='&#171;'>
                             </form>
                             "; 
                         }
                     } else {
                         scan_dir($root_path);
                 }
-                # Render back && back to start END;
+                # Render back && back to start :: END :: ;
                 # Render file content :: START ::
                 function read($file) {
                     if (is_readable($file)) {
-                        echo "<pre>".htmlspecialchars(file_get_contents($file));
+                        echo "<div class='renderFile' id=''style-13'>
+                                <div class='scrollbar' id='style-13'>
+                                    <pre><div class='renderText'>".htmlspecialchars(file_get_contents($file))."</div></pre>
+                                </div>
+                            </div>";
+                            
                     } else {
                         return null;
                     }
@@ -123,19 +144,21 @@
                 }
                 ?>
             <?php if(!isset($_POST['submit'])) { ?>
-                <form action="" method="post">
-                    <input type="text" name="add" placeholder="Name your folder">
-                    <input type="submit" name ='submit'>
+                <form class="create-folder" action="" method="post">
+                    <input class="input-text" type="text" name="add" placeholder="Name your folder">
+                    <input class="input-btn" type="submit" value="Create folder">
+                    <input type="hidden" name ='submit'>
                 </form>
             <?php } else {
                 createDirectory();
                 ob_get_clean();
                 scan_dir($root_path);
             ?>
-            <form action="" method="post">
-                <input type="text" name="add" placeholder="Name your folder">
-                <input type="submit" name ='submit'>
-            </form>
+            <form class="create-folder" action="" method="post">
+                    <input class="input-text" type="text" name="add" placeholder="Name your folder">
+                    <input class="input-btn" type="submit" value="Create folder">
+                    <input type="hidden" name ='submit'>
+                </form>
             <?php } 
             # Create directory && render form :: END :: ;
             ?>
