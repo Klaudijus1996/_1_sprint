bbe762f50def74ad7523051ca07141b57e499bcc
Klaudijus <klaudijus.k1996@gmail.com>
2020-07-28 17:30:47 +0300

    error upd

--- upload.php
+++ upload.php
@@ -1,5 +1,6 @@
 <?php 
     if(isset($_FILES['image'])){
+        $msg;
         $root = getcwd();
         $getPath = $_GET['path'];
         $errors= array();
@@ -10,16 +11,23 @@
         $file_ext = strtolower(end(explode('.',$_FILES['image']['name'])));
         $extensions = array("jpeg","jpg","png","txt","php", "html", "css", "js");
         if(in_array($file_ext,$extensions)=== false){
-            $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+            $errors[]="Extension is not allowed, please choose a different file.";
         }
         if($file_size > 2097152) {
-            $errors[]='File size must be excately 2 MB';
+            $errors[]='File size must be below 2 MB';
         }
         if(empty($errors)==true) {
             move_uploaded_file($file_tmp, $root.$getPath.'/'.$file_name);
             
-        }else{
-            print_r($errors);
+        }
+        else{ ?> <div class="upload-error"> <?php
+            // print_r($errors); 
+            for ($i=0; $i < count($errors); $i++) { 
+                $msg.=$errors[$i];
+            }
+            echo $msg;
+            ?>
+                 </div> <?php
         }
     }
 ?>
